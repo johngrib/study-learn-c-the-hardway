309f132879f6ed15dcacb60609591fd99cd5d6c7
John Grib <johngrib82@gmail.com>
2020-09-27 22:57:02 +0900

    ex1 예제를 학습하라

---
+++ src/ex1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/* 이것은 주석이다. */
+int main(int argc, char *argv[])
+{
+    int distance = 100;
+
+    // 이것도 주석이다.
+    printf("You are %d miles away.\n", distance);
+
+    return 0;
+}
