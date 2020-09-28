4d9c3ab2f7457a5135ed1d028a161c3113afbfe4
John Grib <johngrib82@gmail.com>
2020-09-28 17:14:44 +0900

    모음 판별 코드를 함수로 분리하라

--- src/ex10.c
+++ src/ex10.c
@@ -1,8 +1,46 @@
 #include <stdio.h>
 
+void print_vowel(char letter, int location)
+{
+    switch (letter) {
+        case 'a':
+        case 'A':
+            printf("%d: 'A'\n", location);
+            break;
+
+        case 'e':
+        case 'E':
+            printf("%d: 'E'\n", location);
+            break;
+
+        case 'i':
+        case 'I':
+            printf("%d: 'I'\n", location);
+            break;
+
+        case 'o':
+        case 'O':
+            printf("%d: 'O'\n", location);
+            break;
+
+        case 'u':
+        case 'U':
+            printf("%d: 'U'\n", location);
+            break;
+
+        case 'y':
+        case 'Y':
+            printf("%d: 'Y'\n", location);
+            break;
+
+        default:
+            printf("%d: %c is not a vowel\n", location, letter);
+    }
+}
+
 int main(int argc, char *argv[])
 {
-    if (argc != 2) {
+    if (argc < 2) {
         printf("ERROR: You need one argument.\n");
         return 1;
     }
@@ -10,42 +48,7 @@ int main(int argc, char *argv[])
     int i = 0;
     for (i = 0; argv[1][i] != '\0'; i++) {
         char letter = argv[1][i];
-
-        switch (letter) {
-            case 'a':
-            case 'A':
-                printf("%d: 'A'\n", i);
-                break;
-
-            case 'e':
-            case 'E':
-                printf("%d: 'E'\n", i);
-                break;
-
-            case 'i':
-            case 'I':
-                printf("%d: 'I'\n", i);
-                break;
-
-            case 'o':
-            case 'O':
-                printf("%d: 'O'\n", i);
-                break;
-
-            case 'u':
-            case 'U':
-                printf("%d: 'U'\n", i);
-                break;
-
-            case 'y':
-            case 'Y':
-                printf("%d: 'Y'\n", i);
-                break;
-
-            default:
-                printf("%d: %c is not a vowel\n", i, letter);
-        }
+        print_vowel(letter, i);
     }
     return 0;
 }
-
